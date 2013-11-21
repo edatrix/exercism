7d63d15097bddde7e04a0dad4e4f14b75a21293c
edatrix <lespath@gmail.com>
2013-11-20 20:32:44 -0700

    grade-school

--- grade-school/grade-school_test.rb
+++ grade-school/grade-school_test.rb
@@ -24,7 +24,7 @@ class SchoolTest < MiniTest::Unit::TestCase
   end
 
   def test_add_more_students_in_same_class
-    skip
+    # skip
     school.add("James", 2)
     school.add("Blair", 2)
     school.add("Paul", 2)
@@ -32,14 +32,14 @@ class SchoolTest < MiniTest::Unit::TestCase
   end
 
   def test_add_students_to_different_grades
-    skip
+    # skip
     school.add("Chelsea", 3)
     school.add("Logan", 7)
     assert_equal({3 => ["Chelsea"], 7 => ["Logan"]}, school.db)
   end
 
   def test_get_students_in_a_grade
-    skip
+    # skip
     school.add("Franklin", 5)
     school.add("Bradley", 5)
     school.add("Jeff", 1)
@@ -47,12 +47,12 @@ class SchoolTest < MiniTest::Unit::TestCase
   end
 
   def test_get_students_in_a_non_existant_grade
-    skip
+    # skip
     assert_equal [], school.grade(1)
   end
 
   def test_sort_school
-    skip
+    # skip
     school.add("Jennifer", 4)
     school.add("Kareem", 6)
     school.add("Christopher", 4)
