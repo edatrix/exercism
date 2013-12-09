57069f0a3f6d38cedd1ea04efd911f7ce151530c
edatrix <lespath@gmail.com>
2013-12-08 22:43:19 -0700

    starting anagram

---
+++ anagram/anagram.rb
@@ -0,0 +1,11 @@
+class Anagram
+
+  def initialize(input)
+    @input = input.split("")
+  end
+
+  def match(possible_matches)
+    @input
+  end
+
+end
--- word-count/phrase.rb
+++ word-count/phrase.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Phrase
 
   def initialize(phrase)
