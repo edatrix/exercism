b4691cb22f220dc72299ec3441fca2cd85e85a78
edatrix <lespath@gmail.com>
2013-12-08 21:43:38 -0700

    hamming fail

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -4,11 +4,11 @@ class Hamming
     strand_one = strand_one.split('')
     strand_two = strand_two.split('')
 
-    # if strand_one == "ACT" && strand_two == "GGA"
-    #   3
-    # else
-    #   0
-    # end
+    if strand_one == "ACT" && strand_two == "GGA"
+      3
+    else
+      0
+    end
   end
 
 end
