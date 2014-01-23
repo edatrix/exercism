fcdca24ce367b6d81da3f6d8ce77623cffe262d6
edatrix <lespath@gmail.com>
2014-01-23 14:55:43 -0700

    anagram passes

--- anagram/anagram_test.rb
+++ anagram/anagram_test.rb
@@ -11,60 +11,60 @@ class AnagramTest < MiniTest::Unit::TestCase
   end
 
   def test_detect_simple_anagram
-    skip
+    # skip
     detector = Anagram.new('ant')
     anagrams = detector.match(['tan', 'stand', 'at'])
     assert_equal ['tan'], anagrams
   end
 
   def test_detect_multiple_anagrams
-    skip
+    # skip
     detector = Anagram.new('master')
     anagrams = detector.match(['stream', 'pigeon', 'maters'])
     assert_equal ['maters', 'stream'], anagrams.sort
   end
 
   def test_does_not_confuse_different_duplicates
-    skip
+    # skip
     detector = Anagram.new('galea')
     assert_equal [], detector.match(['eagle'])
   end
 
   def test_identical_word_is_not_anagram
-    skip
+    # skip
     detector = Anagram.new('corn')
     anagrams = detector.match %w(corn dark Corn rank CORN cron park)
     assert_equal ['cron'], anagrams
   end
 
   def test_eliminate_anagrams_with_same_checksum
-    skip
+    # skip
     detector = Anagram.new('mass')
     assert_equal [], detector.match(['last'])
   end
 
   def test_eliminate_anagram_subsets
-    skip
+    # skip
     detector = Anagram.new('good')
     assert_equal [], detector.match(['dog', 'goody'])
   end
 
   def test_detect_anagram
-    skip
+    # skip
     detector = Anagram.new('listen')
     anagrams = detector.match %w(enlists google inlets banana)
     assert_equal ['inlets'], anagrams
   end
 
   def test_multiple_anagrams
-    skip
+    # skip
     detector = Anagram.new('allergy')
     anagrams = detector.match %w(gallery ballerina regally clergy largely leading)
     assert_equal ['gallery', 'largely', 'regally'], anagrams.sort
   end
 
   def test_anagrams_are_case_insensitive
-    skip
+    # skip
     detector = Anagram.new('Orchestra')
     anagrams = detector.match %w(cashregister Carthorse radishes)
     assert_equal ['Carthorse'], anagrams
