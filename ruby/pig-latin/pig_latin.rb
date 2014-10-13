117c66c89f24985a54dc8e3aef110dacf50ede97
edatrix <lespath@gmail.com>
2014-10-12 20:59:42 -0600

    small refactoring, gets rid of self.initialize

--- ruby/pig-latin/pig_latin.rb
+++ ruby/pig-latin/pig_latin.rb
@@ -1,47 +1,48 @@
 class PigLatin
 
-  def self.initialize(word)
-    @first_letter = word.chars.to_a[0]
-    @second_letter = word.chars.to_a[1]
-    @third_letter = word.chars.to_a[2]
+  def initialize(word)
+    @first_letter = word[0]
+    @second_letter = word[1]
+    @third_letter = word[2]
   end
 
   def self.translate(word)
+
     if word.start_with?("a", "e", "i", "o", "u", "yt", "xr")
-      self.vowel_rearrange(word)
+      self.new(word).vowel_rearrange(word)
     elsif word.include?(" ")
-      self.split_phrase(word)
+      self.new(word).split_phrase(word)
     elsif word.start_with?("thr")
       "ushthray"
     elsif word.start_with?("ch", "qu", "th")
-      self.rearrange_two(word)
+      self.new(word).rearrange_two(word)
     elsif word.start_with?("squ", "sch")
-      self.rearrange_three(word)
+      self.new(word).rearrange_three(word)
     else
-      self.basic_rearrange(word)
+      self.new(word).basic_rearrange(word)
     end
   end
 
-  def self.vowel_rearrange(word)
+  def vowel_rearrange(word)
     word.to_s + "ay"
   end
 
-  def self.basic_rearrange(word)
-    self.initialize(word)
+  def basic_rearrange(word)
+    initialize(word)
     word.delete(@first_letter.to_s) + @first_letter.to_s + "ay"
   end
 
-  def self.rearrange_two(word)
-    self.initialize(word)
+  def rearrange_two(word)
+    initialize(word)
     word.delete(@first_letter.to_s).delete(@second_letter.to_s) + @first_letter.to_s + @second_letter.to_s + "ay"
   end
 
-  def self.rearrange_three(word)
-    self.initialize(word)
+  def rearrange_three(word)
+    initialize(word)
     word.delete(@first_letter.to_s).delete(@second_letter.to_s).delete(@third_letter.to_s) + @first_letter.to_s + @second_letter.to_s + @third_letter.to_s + "ay"
   end
 
-  def self.split_phrase(phrase)
+  def split_phrase(phrase)
     phrase_array = phrase.split
     a = phrase_array[0]
     b = phrase_array[1]
